4d7de660417a41e6222a71eb27e0c9e27df7dd70
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-29 12:49:58 -0700

    using useEffect for loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,24 @@ import IngredientList from './IngredientList';
 
 function Ingredients() {
   const [userIngredient, setUserIngredient] = useState([])
+
+  useEffect(()=>{
+    fetch('https://react-hooks-cb0d7.firebaseio.com/ingredients.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(responseData => {
+      const loadedIngredients = [];
+      for(const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredient(loadedIngredients);
+    });
+  },[])
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-cb0d7.firebaseio.com/ingredients.json',{
@@ -21,8 +39,7 @@ function Ingredients() {
             ...prevIngredients,
             {id: responseData.name, ...ingredient}
           ])
-        });
-    
+        });    
   };
 
   const removeIngredientHandler = ingredientId => {
