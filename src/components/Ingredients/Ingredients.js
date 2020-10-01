ee9ecef71771ff61fe343fe2e7bc396d1298c32e
aleksandGG <aleksandrgaranin89@gmail.com>
2020-10-01 12:32:44 -0700

    Authentication with useContext()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useReducer } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
