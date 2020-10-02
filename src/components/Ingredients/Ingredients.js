3830ffc8d0a82705d38a36e8e49b8548164fd926
aleksandGG <aleksandrgaranin89@gmail.com>
2020-10-01 17:30:05 -0700

    search useHttp

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,75 +1,74 @@
-import React, { useCallback, useReducer, useMemo, useEffect } from 'react';
+import React, { useReducer, useEffect, useCallback, useMemo } from 'react';
 
 import IngredientForm from './IngredientForm';
-import Search from './Search';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
-
+import Search from './Search';
 import useHttp from '../../hooks/http';
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
     case 'SET':
       return action.ingredients;
-    case 'ADD': 
+    case 'ADD':
       return [...currentIngredients, action.ingredient];
     case 'DELETE':
       return currentIngredients.filter(ing => ing.id !== action.id);
     default:
       throw new Error('Should not get there!');
   }
-}
+};
 
+const Ingredients = () => {
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifer,
+    clear
+  } = useHttp();
 
-
-const Ingredients = () => {  
-  const [ userIngredients, dispatch ] = useReducer(ingredientReducer, []);
-  const { 
-    isLoading, 
-    error, 
-    data, 
-    sendRequest, 
-    reqExtra, 
-    reqIdentifier,
-    clear } = useHttp();
-
-  useEffect(()=> {
-    if (!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT'){
-      dispatch({type: 'DELETE', id: reqExtra })
-    } else if (!isLoading && !error && reqIdentifier === 'ADD_INGREDIENT') {
+  useEffect(() => {
+    if (!isLoading && !error && reqIdentifer === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifer === 'ADD_INGREDIENT') {
       dispatch({
         type: 'ADD',
-        ingredient: {id: data.name, ...reqExtra } 
+        ingredient: { id: data.name, ...reqExtra }
       });
     }
-  }, [data, reqExtra, reqIdentifier, isLoading, error]);
-  
-  const filterUserIngredintsHandler = useCallback(filteredIngredients => {    
-    dispatch({type: 'SET', ingredients: filteredIngredients});
-  },[])
+  }, [data, reqExtra, reqIdentifer, isLoading, error]);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+  }, []);
 
   const addIngredientHandler = useCallback(ingredient => {
     sendRequest(
-      'https://react-hooks-cb0d7.firebaseio.com/ingredients.json', 
-      'POST', 
-      JSON.stringify(ingredient), 
-      ingredient, 
+      'https://react-hooks-cb0d7.firebaseio.com/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
       'ADD_INGREDIENT'
-    );    
-  },[sendRequest]);
-
-  const removeIngredientHandler = useCallback(ingredientId => {  
-    sendRequest(
-      `https://react-hooks-cb0d7.firebaseio.com/ingredients/${ingredientId}.json`,
-      'DELETE',
-      null, 
-      ingredientId, 
-      'REMOVE_INGREDIENT' 
     );
-  
-  },[sendRequest]);
+  }, [sendRequest]);
 
-  
+  const removeIngredientHandler = useCallback(
+    ingredientId => {
+      sendRequest(
+        `https://react-hooks-cb0d7.firebaseio.com/ingredients/${ingredientId}.json`,
+        'DELETE',
+        null,
+        ingredientId,
+        'REMOVE_INGREDIENT'
+      );
+    },
+    [sendRequest]
+  );
+
   const ingredientList = useMemo(() => {
     return (
       <IngredientList
@@ -80,15 +79,20 @@ const Ingredients = () => {
   }, [userIngredients, removeIngredientHandler]);
 
   return (
-    <div className="App"> 
-      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}     
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+    <div className="App">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
+
       <section>
-        <Search onLoadIngredients={filterUserIngredintsHandler}/>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         {ingredientList}
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
